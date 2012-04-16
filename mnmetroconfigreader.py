b45e055a0809cc31f075b387569a2bad0009ef1d
owenam <owenam@gmail.com>
2012-04-16 10:34:07 -0500

    listing corridors

--- mnmetroconfigreader.py
+++ mnmetroconfigreader.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python
 from __future__ import division
-
+import xml.etree.cElementTree as ET
 
 class MNMetroConfigReader:
-    def __init__(self):
+    def __init__(self, filename=None):
+        if filename == None:
+            self.filename = None
+        else:
+            self.load_file(filename)
+
+    def load_file(self, filename):
+        self.root = ET.parse(filename).getroot()
+
+    def list_corridors(self):
+        '''
+        Returns a list of tuples (route, dir) for each corridor in the metro_config file
+        '''
+        return list((corridor.get('route'), corridor.get('dir')) for corridor in self.root.findall('corridor'))
+
+    def list_rnodes_in_corridor(self, route, dir):
         pass
 
 if __name__ == '__main__':
-    pass
+    testfile = "test/metro_config.xml"
+
+    mcreader = MNMetroConfigReader(testfile)
+    print mcreader.list_corridors()
