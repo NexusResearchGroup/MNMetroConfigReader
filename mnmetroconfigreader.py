01e98c7ff345a690b82825a8ea0afba6e0245cf3
owenam <owenam@gmail.com>
2012-04-16 11:04:46 -0500

    listing r_nodes

--- mnmetroconfigreader.py
+++ mnmetroconfigreader.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 from __future__ import division
+from collections import namedtuple
 import xml.etree.cElementTree as ET
+
+Corridor = namedtuple('Corridor',
+                      ["route",
+                       "dir"]
+                      )
+R_Node = namedtuple('R_Node',
+                    ["name",
+                     "n_type",
+                     "label",
+                     "lat",
+                     "lon",
+                     "lanes",
+                     "shift",
+                     "station_id",
+                     "speed_limit",
+                     "attach_side"]
+                    )
 
 class MNMetroConfigReader:
     def __init__(self, filename=None):
@@ -14,15 +32,36 @@ class MNMetroConfigReader:
 
     def list_corridors(self):
         '''
-        Returns a list of tuples (route, dir) for each corridor in the metro_config file
+        Returns a list of Corridors in the metro_config file
         '''
-        return list((corridor.get('route'), corridor.get('dir')) for corridor in self.root.findall('corridor'))
+        return list(Corridor(route=corridor.get('route'), dir=corridor.get('dir')) for corridor in self.root.findall('corridor'))
 
-    def list_rnodes_in_corridor(self, route, dir):
-        pass
+    def list_rnodes_in_corridor(self, corridor):
+        '''
+        Returns a list containing an R_Node for each r_node in the specified Corridor
+        '''
+        corridor_node = self.root.findall("corridor[@route='%s'][@dir='%s']" % (corridor.route, corridor.dir))[0]
+        r_nodes = []
+        for r_node in corridor_node.findall("r_node"):
+            r_nodes.append(R_Node(name = r_node.get("name"),
+                                  n_type = r_node.get("n_type"),
+                                  label = r_node.get("label"),
+                                  lat = r_node.get("lat"),
+                                  lon = r_node.get("lon"),
+                                  lanes = r_node.get("lanes"),
+                                  shift = r_node.get("shift"),
+                                  station_id = r_node.get("station_id"),
+                                  speed_limit = r_node.get("s_limit"),
+                                  attach_side = r_node.get("attach_side")
+                                  )
+                           )
+        return r_nodes
 
 if __name__ == '__main__':
     testfile = "test/metro_config.xml"
 
     mcreader = MNMetroConfigReader(testfile)
-    print mcreader.list_corridors()
+    corridors = mcreader.list_corridors()
+    rnodes = mcreader.list_rnodes_in_corridor(corridors[0])
+    print corridors
+    print rnodes
